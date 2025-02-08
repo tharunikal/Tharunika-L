eeffffc14afd63babe26d774a86d521a456caa80
Tharunika L <tharunika.l14@gmail.com>
2025-02-08 23:06:33 +0530

    updated the hello text

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './Header.css';
 
 const Header = () => {
+  useEffect(() => {
+    const letters = document.querySelectorAll('.header h1 span');
+    letters.forEach((letter, index) => {
+      setTimeout(() => {
+        letter.classList.add('visible');
+      }, 50 * index);
+    });
+  }, []);
+
   return (
     <header className="header">
-      <h1>Hey, I'm Tharunika L  </h1>
-      <p>Welcome to my portfolio</p>
+      <h1 aria-label="Hello">
+        <span>H</span>
+        <span>e</span>
+        <span>l</span>
+        <span>l</span>
+        <span>o</span>
+      </h1>
+      <p>nice to meet you, this is Tharunika L.</p>
     </header>
   );
 };
